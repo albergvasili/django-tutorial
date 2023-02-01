a5b18a2a078b379d7b448b812867da58fef121f6
albergvasili <albergvasili@yandex.com>
2023-01-31 19:04:32 -0500

    Write more views
    -Add detail, results, and vote views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
 
 def index(req):
     return HttpResponse('Toki, ale. Ni li polls index')
+
+
+def detail(req, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(req, question_id):
+    res = f'You are looking at results of question {question_id}'
+    return HttpResponse(res)
+
+
+def vote(req, question_id):
+    return HttpResponse(f'You are voting on question {question_id}')
