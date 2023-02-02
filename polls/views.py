0e52bd8e68c8c7ec668b8a5863154dab49fda199
albergvasili <albergvasili@yandex.com>
2023-02-01 21:02:16 -0500

    Configure the polls URLconfig to use generic views
    -Use ListView and Detailview to refactor index, detail and result

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(req):
-    latest_questions_list = Question.objects.order_by('pub_date')[:5]
-    context = {
-            'latest_questions_list': latest_questions_list
-            }
-    return render(req, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_questions_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(req, question_id):
@@ -36,5 +37,5 @@ def vote(req, question_id):
         selected_choice.votes += 1
         selected_choice.save()
     return HttpResponseRedirect(
-            reverse('polls:results', args=(question.id,))
+            reverse('polls:results', args=(question.id))
             )
